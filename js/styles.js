f563ff728bee9075558adb9a8643f2033002ff50
chircancristi <cristianchircan1@gmail.com>
2018-03-12 17:35:46 +0200

    Log In page

---
+++ js/styles.js
@@ -0,0 +1,17 @@
+function unhide(divID, divID2) {
+		var item = document.getElementById(divID);
+		var item2 = document.getElementById(divID2);
+		item.className = 'logIn';
+		item2.cla1ssName = 'signIn';
+
+
+    }
+ 
+function proprieties()
+{
+
+var aux2=window.innerHeight/2;
+document.getElementById("formularContainer").style.height=window.innerHeight+'px';
+document.getElementById("formularId").style.height=aux2+'px';
+
+}
