62adbfe31823f52624108697be3d8859186790a1
chircancristi <cristianchircan1@gmail.com>
2018-05-21 15:40:43 +0300

    Log in, functional

--- app/Controller/login.php
+++ app/Controller/login.php
@@ -3,17 +3,24 @@
 
     $servername = "localhost";
     $username = "root";
-    $password = "your_password";
+    $password = "";
     $dbname = "sundaybrawl";
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if (mysqli_connect_errno())
+    {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
     // Check connection
     if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
     }
-    $sql = "SELECT password FROM user where username=".$_POST["username"] ;
+    $sql = "SELECT password FROM user where username = '".$_POST["username"]."'";
+   
     $result = mysqli_query($conn, $sql);
-
+    if ($result==NULl){
+        die("Query error, conection is bad");
+    }
     if (mysqli_num_rows($result) > 0) {
         // output data of each row
         while($row = mysqli_fetch_assoc($result)) {
@@ -21,6 +28,9 @@
             if ( $_POST["password"]==$row["password"]){
                 header("Location: ../View/Pages/dashboard.html");
                 }
+                else {
+                    echo "Razvan avem nevoie de login fail trg ";
+                }    
             
         }
     } else {
