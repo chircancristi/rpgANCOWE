6e28420fabec508d6ecde984b8906e1f714916e8
anggabard <angel.ardeleanu@hotmail.com>
2018-06-12 00:53:32 +0300

    presentation

--- app/Controller/user/search.php
+++ app/Controller/user/search.php
@@ -1,28 +1,21 @@
 <?php
-// required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
  
-// include database and object files
 include_once '../../Config/database.php';
 include_once '../../Model/user.php';
  
-// instantiate database and product object
 $database = new Database();
 $db = $database->getConnection();
  
-// initialize object
 $user = new user($db);
  
-// get keywords
 
 $where = "";
 
-
 if(isset($_GET["username"])){
-    $where.= " username like '%".$_GET["username"]."%'";
+    $where .= " username like '%".$_GET["username"]."%'";
 }
-
 
 if(isset($_GET["moneyBoundUpp"]) || isset($_GET["moneyBoundLow"])){
     if(strcmp($where,"") != 0){
@@ -65,50 +58,56 @@ if(isset($_GET["winsBoundUpp"]) || isset($_GET["winsBoundLow"])){
 }
 
 if(isset($_GET["lossesBoundUpp"]) || isset($_GET["lossesBoundLow"])){
+    if(strcmp($where,"") != 0){
+        $where .= " and";
+    }
     if(isset($_GET["lossesBoundUpp"]) && isset($_GET["lossesBoundLow"])){
-        $where .= " and losses between " . $_GET["lossesBoundLow"] . " and " . $_GET["lossesBoundUpp"];
+        $where .= " losses between " . $_GET["lossesBoundLow"] . " and " . $_GET["lossesBoundUpp"];
     } else if (isset($_GET["lossesBoundUpp"])){
-        $where .= " and losses <= ". $_GET["lossesBoundUpp"];
+        $where .= " losses <= ". $_GET["lossesBoundUpp"];
     } else {
-        $where .= " and losses >= ". $_GET["lossesBoundLow"];
+        $where .= " losses >= ". $_GET["lossesBoundLow"];
     }
 } else {
     if(isset($_GET["losses"])){
-        $where.= " and losses = " . $_GET["losses"];
+        if(strcmp($where,"") != 0){
+            $where .= " and";
+        }
+        $where.= " losses = " . $_GET["losses"];
     }
 }
 
 if(isset($_GET["gamesPlayedBoundUpp"]) || isset($_GET["gamesPlayedBoundLow"])){
+    if(strcmp($where,"") != 0){
+        $where .= " and";
+    }
     if(isset($_GET["gamesPlayedBoundUpp"]) && isset($_GET["gamesPlayedBoundLow"])){
-        $where .= " and gamesPlayed between " . $_GET["gamesPlayedBoundLow"] . " and " . $_GET["gamesPlayedBoundUpp"];
+        $where .= " gamesPlayed between " . $_GET["gamesPlayedBoundLow"] . " and " . $_GET["gamesPlayedBoundUpp"];
     } else if (isset($_GET["gamesPlayedBoundUpp"])){
-        $where .= " and gamesPlayed <= ". $_GET["gamesPlayedBoundUpp"];
+        $where .= " gamesPlayed <= ". $_GET["gamesPlayedBoundUpp"];
     } else {
-        $where .= " and gamesPlayed >= ". $_GET["gamesPlayedBoundLow"];
+        $where .= " gamesPlayed >= ". $_GET["gamesPlayedBoundLow"];
     }
 } else {
     if(isset($_GET["gamesPlayed"])){
-        $where.= " and gamesPlayed = " . $_GET["gamesPlayed"];
+        if(strcmp($where,"") != 0){
+            $where .= " and";
+        }
+        $where.= " gamesPlayed = " . $_GET["gamesPlayed"];
     }
 }
-// query products
+
 $stmt = $user->search($where);
 $num = $stmt->rowCount();
  
-// check if more than 0 record found
 if($num>0){
  
-    // products array
     $users_arr=array();
     $users_arr["records"]=array();
  
-    // retrieve our table contents
-    // fetch() is faster than fetchAll()
-    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        // extract row
-        // this will make $row['name'] to
-        // just $name only
+
         extract($row);
  
         $single_user=array(
